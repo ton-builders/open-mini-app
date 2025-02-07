7662ad41546262e55a4e78861abeec912bb18208
Andrew <andrew@Andrews-MacBook-Air.local>
2025-02-07 17:24:50 +0800

    recover

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,8 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
-export default {
-  plugins: [tailwindcss(), autoprefixer()],
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+  },
 };
+
+export default config;
